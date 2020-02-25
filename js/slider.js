ff89d4e746b6fc531427b6239d03e8186ea31830
venaaa <idaveee@gmail.com>
2020-02-25 21:50:58 +0300

    Added NAVBAR, HEADER, CALC and ABOUT. Styles, bootstrap and owlcarousel

---
+++ js/slider.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+});
+
+// MAIN SLIDER
+$('.carousel-main').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    },
+    dots:false,
+    mouseDrag:false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+})
